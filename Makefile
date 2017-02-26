bb460aa65ebccb089339907c110f9f335ae401fe
Will Dicharry <wdicharry@gmail.com>
2017-02-26 13:36:50 -0700

    Starts at correct address, visible in gdb.

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 CROSS_COMPILE=arm-none-eabi-
-OBJCOPY=$(CROSS_COMPILE)objcopy
 
-TARGET_NAME=thumbv7m-none-eabi
+# TARGET_NAME=thumbv7m-none-eabi
 # Raspberry Pi:
-#TARGET_NAME=arm-unknown-linux-gnueabihf
+TARGET_NAME=arm-unknown-linux-gnueabihf
+
+OBJCOPY=$(CROSS_COMPILE)objcopy
+OBJDUMP=$(CROSS_COMPILE)objdump
 
 BUILD_PREFIX=target/$(TARGET_NAME)
 DEBUG_BUILD_PREFIX=$(BUILD_PREFIX)/debug
@@ -13,7 +15,7 @@ KERNEL_IMG=$(DEBUG_BUILD_PREFIX)/kernel.img
 
 all: $(KERNEL_IMG)
 
-$(KERNEL_ELF): src/main.rs
+$(KERNEL_ELF): src/main.rs .cargo/config ld/raspi.ld
 	xargo build --target $(TARGET_NAME)
 
 $(KERNEL_IMG): $(KERNEL_ELF)
@@ -21,6 +23,15 @@ $(KERNEL_IMG): $(KERNEL_ELF)
 
 core:
 	rustup target add $(TARGET_NAME)
+
+qemu: $(KERNEL_IMG)
+	qemu-system-arm -M raspi2 --nographic -bios $(KERNEL_IMG)
+
+qemu-gdb: $(KERNEL_IMG)
+	qemu-system-arm -M raspi2 --nographic -s -S -bios $(KERNEL_IMG)
+
+kerneldump:
+	$(OBJDUMP) -CD $(KERNEL_ELF)
 
 clean:
 	xargo clean
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,27 @@
 #![feature(lang_items)]
-
-// We won't use the usual `main` function. We are going to use a different
-// "entry point".
 #![no_main]
-
-// We won't use the standard library because it requires OS abstractions like
-// threads and files and those are not available on this platform.
 #![no_std]
 
-// Conceptually, this is our program "entry point". It's the first thing the
-// microcontroller will execute when it (re)boots. This entry point must be a
-// `pub`lic function named `_reset` to be recognized as such because that's what
-// our linker script (`layout.ld`) states. Later, we'll say more about these
-// requirements.
-//
-// Also, returning from this function is undefined because there is nothing to
-// return to! To statically forbid returning from this function, we mark it as
-// "divergent", hence the `fn() -> !` signature.
-#[export_name = "_reset"]
-pub extern "C" fn main() -> ! {
-    // Our first program initializes some variables on the stack and does
-    // nothing more. Yay!
-    let y;
-    let x = 42;
-    y = x;
+const RPI_PERIPHERAL_BASE: u32 = 0x3f000000;
+const RPI_UART0_BASE: u32 = RPI_PERIPHERAL_BASE + 0x00201000;
 
-    // We can't return from this function so we just spin endlessly here.
-    loop {}
+fn print_uart0() {
+  let addr = RPI_UART0_BASE as *mut char;
+  unsafe { *addr = 'R' }
 }
 
-// Finally, we need to define the panic_fmt "lang item", which is just a
-// function. This specifies what the program should do when a `panic!` occurs.
-// Our program won't panic, so we can leave the function body empty for now.
+#[export_name = "_reset"]
+pub extern "C" fn main() -> ! {
+  let x = 42;
+  let y = x;
+
+  print_uart0();
+  loop {}
+}
+
 mod lang_items {
-    #[lang = "panic_fmt"]
-    extern "C" fn panic_fmt() {}
+  #[lang = "panic_fmt"]
+  extern "C" fn panic_fmt() {}
+  #[lang = "eh_personality"]
+  extern "C" fn eh_personality() {}
 }
